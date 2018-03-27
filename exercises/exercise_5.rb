444e62a2cb9db7b73a40a53694d4f8e4f40df579
Di Wu <wd1991610@gmail.com>
2018-03-27 21:02:30 +0000

    finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+store_revenue_sum = Store.sum("annual_revenue")
+puts"Sum of all Store Revnue: #{store_revenue_sum}"
+
+store_average_revenue=Store.sum("annual_revenue")/Store.count
+puts "Average Revenue Per Store: #{store_average_revenue}"
+
+number_of_stores_over_1M= Store.where("annual_revenue > 1000000").count
+puts "number of stores that revenue over 1M: #{number_of_stores_over_1M}"
