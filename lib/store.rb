1d35682fb65cfd5d121187a7a786c186607df93b
Di Wu <wd1991610@gmail.com>
2018-03-27 23:37:19 +0000

    fixed a validation bug

--- lib/store.rb
+++ lib/store.rb
@@ -2,7 +2,7 @@ class Store < ActiveRecord::Base
     has_many :employees
 
     validates :name, length:{minimum:3}
-    validates :annual_revenue, numericality:{greater_than_or_equal_to:3}
+    validates :annual_revenue, numericality:{greater_than_or_equal_to:40, lesser_than_or_equal_to:200}
     validate :at_least_one_apparel, on: :create
 
     def at_least_one_apparel
